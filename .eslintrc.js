6bd22d1817354f6fbb5c121d139d13f5322ea37c
portlogics-mingyukwak <mg.kwak@portlogics.com>
2025-03-19 20:24:57 +0900

    build: npm to pnpm

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,11 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    requireConfigFile: false,
+    babelOptions: {
+      presets: ['module:metro-react-native-babel-preset'],
+    },
+  },
 };
