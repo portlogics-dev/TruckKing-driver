240b432e8f435526df8dc632ccb3fbadd272ba40
portlogics-mingyukwak <mg.kwak@portlogics.com>
2025-03-26 13:39:38 +0900

    chore: translation 추가

---
+++ scripts/check-translations.ts
@@ -0,0 +1,18 @@
+import { run } from "./translations";
+import { supportLanguages } from "../src/i18n/config";
+import { Key } from "../src/i18n/key";
+
+// 키 파일과 번역 파일들의 경로
+const TRANSLATIONS_DIR = "src/i18n/locales";
+
+function main() {
+  run({
+    projectName: "trucklink",
+    keyObject: Key,
+    translationsDir: TRANSLATIONS_DIR,
+    supportedLanguages: supportLanguages,
+    autoRemove: false,
+  });
+}
+
+main();
