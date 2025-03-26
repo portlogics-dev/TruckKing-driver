b8a98005594bf49fb8c4069c4710c14056f51aff
portlogics-mingyukwak <mg.kwak@portlogics.com>
2025-03-26 11:23:53 +0900

    chore: lint settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,83 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
-  parser: '@babel/eslint-parser',
+  parser: "@typescript-eslint/parser",
+  extends: [
+    "@react-native-community",
+    "eslint:recommended",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  plugins: ["import", "@typescript-eslint", "prettier"],
+  settings: {
+    "import/parsers": {
+      "@typescript-eslint/parser": [".ts", ".tsx"],
+    },
+    "import/resolver": {
+      typescript: {
+        project: "./tsconfig.json",
+      },
+    },
+    react: {
+      version: "detect",
+    },
+  },
+  rules: {
+    // import
+    "import/order": [
+      "error",
+      {
+        groups: [
+          ["builtin", "external"],
+          "internal",
+          ["parent", "sibling"],
+          "index",
+        ],
+        "newlines-between": "always",
+        pathGroups: [
+          {
+            pattern: "@/**",
+            group: "internal",
+            position: "after",
+          },
+        ],
+        alphabetize: {
+          order: "asc",
+          caseInsensitive: true,
+        },
+      },
+    ],
+    // prettier
+    "prettier/prettier": [
+      "warn",
+      {
+        endOfLine: "auto",
+      },
+    ],
+    // typescript-unused-vars
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      {
+        args: "all",
+        argsIgnorePattern: "^_",
+        caughtErrors: "all",
+        caughtErrorsIgnorePattern: "^_",
+        destructuredArrayIgnorePattern: "^_",
+        varsIgnorePattern: "^_",
+        ignoreRestSiblings: true,
+      },
+    ],
+  },
+  ignorePatterns: [
+    // Ignore dotfiles
+    ".*.js",
+    "node_modules/",
+  ],
   parserOptions: {
     requireConfigFile: false,
     babelOptions: {
-      presets: ['module:metro-react-native-babel-preset'],
+      presets: ["module:metro-react-native-babel-preset"],
     },
   },
 };
--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,7 @@
 module.exports = {
-  arrowParens: 'avoid',
-  bracketSameLine: true,
-  bracketSpacing: false,
-  singleQuote: true,
-  trailingComma: 'all',
+  singleQuote: false,
+  semi: true,
+  useTabs: false,
+  tabWidth: 2,
+  printWidth: 80,
 };
