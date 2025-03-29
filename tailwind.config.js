76cf095a5498552b0b0914ef86dfd050514a976f
portlogics-mingyukwak <mg.kwak@portlogics.com>
2025-03-29 18:44:53 +0900

    fix: import -> require 지친다지쳐

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
   // NOTE: Update this to include the paths to all of your component files.
   darkMode: "class",
   content: ["./App.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}"],
-  presets: [import("nativewind/preset")],
+  presets: [require("nativewind/preset")],
   theme: {
     extend: {
       colors: {
